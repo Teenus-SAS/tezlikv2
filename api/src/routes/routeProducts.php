30ab4d73d4ec5f97b9f6bd33a09d50980042be09
Sergio Eduardo Velandia <sergio.velandia@gmail.com>
2021-11-16 21:59:40 -0500

    cambio de metodo

--- api/src/routes/routeProducts.php
+++ api/src/routes/routeProducts.php
@@ -9,7 +9,7 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 
 /* Consulta todos */
 
-$app->post('/products', function (Request $request, Response $response, $args) use ($productsDao) {
+$app->get('/products', function (Request $request, Response $response, $args) use ($productsDao) {
     $products = $productsDao->findAllProductsByCompany('44');
     $response->getBody()->write(json_encode($products, JSON_NUMERIC_CHECK));
     return $response->withHeader('Content-Type', 'application/json');
