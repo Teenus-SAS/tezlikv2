4289a63bc5afc76e27ede333784ffbbba40aaa45
Sergio Eduardo Velandia <sergio.velandia@gmail.com>
2022-03-03 18:56:09 -0500

    varibale de sesion compania en nomina

--- api/src/routes/routePayroll.php
+++ api/src/routes/routePayroll.php
@@ -11,8 +11,8 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 
 $app->get('/payroll', function (Request $request, Response $response, $args) use ($payrollDao) {
     session_start();
-    /* $id_company = $_SESSION['empresas_id_empresas']; */
-    $payroll = $payrollDao->findAllPayrollByCompany(44);
+    $id_company = $_SESSION['id_company'];
+    $payroll = $payrollDao->findAllPayrollByCompany($id_company);
     $response->getBody()->write(json_encode($payroll, JSON_NUMERIC_CHECK));
     return $response->withHeader('Content-Type', 'application/json');
 });
