17a5f0b37a2ce9017392ef10abc02051960d6284
Sergio Eduardo Velandia <sergio.velandia@gmail.com>
2022-04-09 17:29:54 -0500

    importar tabla

--- app/js/machines/configMachines.js
+++ app/js/machines/configMachines.js
@@ -1,38 +1,39 @@
-$(document).ready(function() {
-    $(".cardCreateMachines").hide();
+$(document).ready(function () {
+  sessionStorage.removeItem('machinesData');
 
-    $("#btnNewMachine").click(function(e) {
-        e.preventDefault();
-        $(".cardCreateMachines").toggle(800);
-    });
+  $('.cardCreateMachines').hide();
 
-    $.ajax({
-        type: "GET",
-        url: "../../api/machines",
-        success: function(r) {
+  $('#btnNewMachine').click(function (e) {
+    e.preventDefault();
+    $('.cardCreateMachines').toggle(800);
+  });
 
-            machinesData = JSON.stringify(r)
-            sessionStorage.setItem('machinesData', machinesData)
+  $.ajax({
+    type: 'GET',
+    url: '../../api/machines',
+    success: function (r) {
+      machinesData = JSON.stringify(r);
+      sessionStorage.setItem('machinesData', machinesData);
 
-            let $select = $(`#idMachine`);
-            $select.empty();
+      let $select = $(`#idMachine`);
+      $select.empty();
 
-            $select.append(`<option disabled selected>Seleccionar</option>`);
-            $.each(r, function(i, value) {
-                $select.append(
-                    `<option value = ${value.id_machine}> ${value.machine} </option>`
-                );
-            });
+      $select.append(`<option disabled selected>Seleccionar</option>`);
+      $.each(r, function (i, value) {
+        $select.append(
+          `<option value = ${value.id_machine}> ${value.machine} </option>`
+        );
+      });
 
-            // let $select1 = $(`#selectNameProduct`)
-            // $select1.empty()
+      // let $select1 = $(`#selectNameProduct`)
+      // $select1.empty()
 
-            // $select1.append(`<option disabled selected>Seleccionar</option>`)
-            // $.each(r, function(i, value) {
-            //     $select1.append(
-            //         `<option value = ${value.id_material}> ${value.product} </option>`,
-            //     )
-            // })
-        },
-    });
+      // $select1.append(`<option disabled selected>Seleccionar</option>`)
+      // $.each(r, function(i, value) {
+      //     $select1.append(
+      //         `<option value = ${value.id_material}> ${value.product} </option>`,
+      //     )
+      // })
+    },
+  });
 });
--- app/js/products/importProducts.js
+++ app/js/products/importProducts.js
@@ -1,30 +1,49 @@
-$(document).ready(function() {
-    let selectedFile;
+$(document).ready(function () {
+  let selectedFile;
 
-    $('.cardImportProducts').hide();
+  $('.cardImportProducts').hide();
 
-    $('#btnNewImportProducts').click(function(e) {
-        e.preventDefault();
-        $('.cardCreateProduct').hide(800);
-        $('.cardImportProducts').toggle(800);
-    });
+  $('#btnNewImportProducts').click(function (e) {
+    e.preventDefault();
+    $('.cardCreateProduct').hide(800);
+    $('.cardImportProducts').toggle(800);
+  });
 
-    $('#fileProducts').change(function(e) {
-        e.preventDefault();
-        selectedFile = e.target.files[0];
-    });
+  $('#fileProducts').change(function (e) {
+    e.preventDefault();
+    selectedFile = e.target.files[0];
+  });
 
-    $('#btnImportProducts').click(function(e) {
-        e.preventDefault();
+  $('#btnImportProducts').click(function (e) {
+    e.preventDefault();
 
-        file = $('#fileProducts').val();
+    file = $('#fileProducts').val();
 
-        if (!file) {
-            toastr.error('Seleccione un archivo');
-            return false
-        }
+    if (!file) {
+      toastr.error('Seleccione un archivo');
+      return false;
+    }
 
-        importFile(selectedFile)
-    });
+    importFile(selectedFile);
 
+    debugger;
+    rowProducts = importFile();
+  });
+
+  importFile = (selectedFile) => {
+    if (selectedFile) {
+      let fileReader = new FileReader();
+      fileReader.readAsBinaryString(selectedFile);
+      fileReader.onload = (event) => {
+        let data = event.target.result;
+        let workbook = XLSX.read(data, { type: 'binary' });
+        workbook.SheetNames.forEach((sheet) => {
+          let rowObject = XLSX.utils.sheet_to_row_object_array(
+            workbook.Sheets[sheet]
+          );
+          return rowObject;
+        });
+      };
+    }
+  };
 });
