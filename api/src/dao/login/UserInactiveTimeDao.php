63edd2d469fafe0be47176876bb5ace06c3bbc1e
Sergio Eduardo Velandia <sergio.velandia@gmail.com>
2022-04-19 23:06:24 -0500

    redireccion index al finalizar session

--- api/src/dao/login/UserInactiveTimeDao.php
+++ api/src/dao/login/UserInactiveTimeDao.php
@@ -10,7 +10,8 @@ class UserInactiveTimeDao
     @session_start();
     if (empty($_SESSION['active']) || time() - $_SESSION['time'] > 600) {
       session_destroy();
-      header('location: ../../../../../index.php');
+      echo "<script> window.location='http://tezlikv2/'; </script>";
+      exit();
     } else
       @session_start();
   }
