c91ad2f1eccd0188dd20aadc5649baf58c2ac313
Sergio Eduardo Velandia <sergio.velandia@gmail.com>
2022-06-16 15:22:24 -0500

    ajuste intentos de abrir sesion

--- api/src/routes/app/login/routeLogin.php
+++ api/src/routes/app/login/routeLogin.php
@@ -23,107 +23,98 @@ $app->post('/userAutentication', function (Request $request, Response $response,
     $parsedBody = $request->getParsedBody();
 
     /* Validar intentos de sesión */
-    $file = "C:/apps/htdocs/tezlikv2/assets/txt/session.txt";
-    $fo_r = fopen($file, "r");
-    $attempts = intval(fgets($fo_r));
-    fclose($fo_r);
 
-    /* Validar que el archivo existe */
-    if (!$fo_r) {
+    $tries = file_get_contents('tries.txt');
+    settype($tries, 'integer');
+    $max_tries = 5;
 
-        $resp = array('error' => true, 'message' => 'Se produjo un error. Vuelva a intentarlo');
+    if ($tries >= $max_tries) {
+        $resp = array('error' => true, 'message' => 'Número máximo de intentos superados. Vuelva a intentar en unos minutos');
         $response->getBody()->write(json_encode($resp));
         return $response->withStatus(200)->withHeader('Content-Type', 'application/json');
+    }
 
-        /*Validaciones de sesión siempre y cuando sea menor a 4 intentos.*/
-    } elseif ($attempts < 3) {
+    $user = $parsedBody["validation-email"];
+    $password = $parsedBody["validation-password"];
+    $user = $autenticationDao->findByEmail($user);
 
-        $user = $parsedBody["validation-email"];
-        $password = $parsedBody["validation-password"];
-        $user = $autenticationDao->findByEmail($user);
+    /*Validar usuario y contraseña*/
+    if ($user == null || $user['password'] != hash("sha256", $password)) {
 
-        /*Validar usuario y contraseña*/
-        if ($user == null || $user['password'] != hash("sha256", $password)) {
+        $new_trie = ++$tries;
+        file_put_contents('tries.txt', $new_trie);
 
-            $resp = array('error' => true, 'message' => 'Usuario y/o password incorrectos, valide nuevamente');
-
-            /*Contador y tiempo*/
-            $attempts = $attempts + 1;
-            $time = time();
-
-            /*Abrir, sobreescribir archivo con nuevo valor, guardar y cerrar*/
-            $fo_w = fopen($file, 'w');
-            fwrite($fo_w, $attempts . PHP_EOL);
-            fwrite($fo_w, $time);
-            fclose($fo_w);
-
-            $response->getBody()->write(json_encode($resp));
-            return $response->withStatus(200)->withHeader('Content-Type', 'application/json');
-        }
-
-        /* valide licenciamiento empresa */
-
-        $license = $licenseDao->findLicense($user['id_company']);
-
-        if ($license == 0) {
-            $resp = array('error' => true, 'message' => 'Su licencia ha caducado, lo invitamos a comunicarse');
-            $response->getBody()->write(json_encode($resp));
-            return $response->withStatus(200)->withHeader('Content-Type', 'application/json');
-        }
-
-        /* Validar que el usuario es activo */
-
-        if ($user['active'] != 1) {
-            $resp = array('error' => true, 'message' => 'Usuario Inactivo, valide con el administrador');
-            $response->getBody()->write(json_encode($resp));
-            return $response->withStatus(200)->withHeader('Content-Type', 'application/json');
-        }
-
-
-        /* Valida la session del usuario */
-
-        if ($user['session_active'] != 0) {
-            $resp = array('error' => true, 'message' => 'Usuario logeado, cierre la sesión para abrir una nueva');
-            $response->getBody()->write(json_encode($resp));
-            return $response->withStatus(200)->withHeader('Content-Type', 'application/json');
-        }
-
-        /* Nueva session */
-        session_start();
-        $_SESSION['active'] = true;
-        $_SESSION['idUser'] = $user['id_user'];
-        $_SESSION['name'] = $user['firstname'];
-        $_SESSION['lastname'] = $user['lastname'];
-        $_SESSION['email'] = $user['email'];
-        $_SESSION['rol'] = $user["id_rols"];
-        $_SESSION['id_company'] = $user['id_company'];
-        $_SESSION["time"] = time();
-
-        /* Actualizar metodo ultimo logueo */
-        $lastLoginDao->findLastLogin();
-
-
-        /* Genera codigo */
-        //$code = $generateCodeDao->GenerateCode();
-        //$_SESSION["code"] = $code;
-
-        /* Envio el codigo por email */
-        //$sendEmailDao->SendEmailCode($code, $user);
-
-        /* Modificar el estado de la sesion del usuario en BD */
-        //$statusActiveUserDao->changeStatusUserLogin();
-
-        $user["id_rols"] == 1 ? $location = '../../admin/' : $location = '../../app/';
-
-        $resp = array('success' => true, 'message' => 'Ingresar código', 'location' => $location);
+        $resp = array('error' => true, 'message' => 'Usuario y/o password incorrectos, valide nuevamente');
         $response->getBody()->write(json_encode($resp));
         return $response->withStatus(200)->withHeader('Content-Type', 'application/json');
+    }
 
-        /* $resp = array('success' => true, 'message' => 'access granted');
+    /* valide licenciamiento empresa */
+
+    $license = $licenseDao->findLicense($user['id_company']);
+
+    if ($license == 0) {
+        $resp = array('error' => true, 'message' => 'Su licencia ha caducado, lo invitamos a comunicarse');
+        $response->getBody()->write(json_encode($resp));
+        return $response->withStatus(200)->withHeader('Content-Type', 'application/json');
+    }
+
+    /* Validar que el usuario es activo */
+
+    if ($user['active'] != 1) {
+        $resp = array('error' => true, 'message' => 'Usuario Inactivo, valide con el administrador');
+        $response->getBody()->write(json_encode($resp));
+        return $response->withStatus(200)->withHeader('Content-Type', 'application/json');
+    }
+
+
+    /* Valida la session del usuario */
+
+    if ($user['session_active'] != 0) {
+        $resp = array('error' => true, 'message' => 'Usuario logeado, cierre la sesión para abrir una nueva');
+        $response->getBody()->write(json_encode($resp));
+        return $response->withStatus(200)->withHeader('Content-Type', 'application/json');
+    }
+
+    /* Nueva session */
+    $new_trie = 0;
+    file_put_contents('tries.txt', $new_trie);
+
+    session_start();
+    $_SESSION['active'] = true;
+    $_SESSION['idUser'] = $user['id_user'];
+    $_SESSION['name'] = $user['firstname'];
+    $_SESSION['lastname'] = $user['lastname'];
+    $_SESSION['email'] = $user['email'];
+    $_SESSION['rol'] = $user["id_rols"];
+    $_SESSION['id_company'] = $user['id_company'];
+    $_SESSION["time"] = time();
+
+    /* Actualizar metodo ultimo logueo */
+    $lastLoginDao->findLastLogin();
+
+
+    /* Genera codigo */
+    //$code = $generateCodeDao->GenerateCode();
+    //$_SESSION["code"] = $code;
+
+    /* Envio el codigo por email */
+    //$sendEmailDao->SendEmailCode($code, $user);
+
+    /* Modificar el estado de la sesion del usuario en BD */
+    //$statusActiveUserDao->changeStatusUserLogin();
+
+    $user["id_rols"] == 1 ? $location = '../../admin/' : $location = '../../app/';
+
+    $resp = array('success' => true, 'message' => 'Ingresar código', 'location' => $location);
+    $response->getBody()->write(json_encode($resp));
+    return $response->withStatus(200)->withHeader('Content-Type', 'application/json');
+
+    /* $resp = array('success' => true, 'message' => 'access granted');
         $response->getBody()->write(json_encode($resp));
         return $response->withStatus(200)->withHeader('Content-Type', 'application/json'); */
 
-    } else {       
+    /* } else {       
 
         $fichero = $file;
         $filas = file($fichero);
@@ -141,7 +132,7 @@ $app->post('/userAutentication', function (Request $request, Response $response,
         $resp = array('error' => true, 'message' => 'Limite de intentos alcanzado. Inténtelo más tarde.', 'attempts' => true);
         $response->getBody()->write(json_encode($resp));
         return $response->withStatus(200)->withHeader('Content-Type', 'application/json');
-    }
+    } */
 
 
 
